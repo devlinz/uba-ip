1c894bc15a7cc8380cff76a90ef5900e60fbe85a
Devlin <60636510+devlinz@users.noreply.github.com>
2024-04-10 20:44:45 -0300

    Terminado Clase Practica 4, Guia 3 parte 2

---
+++ practica3-2.hs
@@ -0,0 +1,51 @@
+-- 4b
+todoMenor :: (Float, Float) -> (Float, Float) -> Bool
+todoMenor (a,b) (c,d) = (esMenor a c) && (esMenor b d)
+
+esMenor :: Float -> Float -> Bool
+esMenor x y = x < y
+
+-- 4f
+posPrimerPar :: (Int, Int, Int) -> Int
+posPrimerPar (x,y,z) 
+    | esPar x = 1
+    | esPar y = 2
+    | esPar z = 3
+    | otherwise = 4
+
+esPar :: Int -> Bool
+esPar x = mod x 2 == 0
+
+-- 6: Error bisiesto 1900 = False, bisiesto 2000 = True
+bisiesto :: Int -> Bool
+bisiesto x = (mod x 4 == 0) || ((mod x 100 == 0) || (mod x 400 /= 0))
+
+-- 7
+distanciaManhattan :: (Float, Float, Float) -> (Float, Float, Float) -> Float
+distanciaManhattan (a,b,c) (x,y,z) = absoluto (a-x) + absoluto (b-y) + absoluto (c-z)
+
+absoluto :: Float -> Float
+absoluto x 
+    | x < 0.0 = -x
+    | otherwise = x
+
+-- 8
+comparar :: Int -> Int -> Int
+comparar x y
+    | sumaUltimos2Digitos x > sumaUltimos2Digitos y = -1
+    | sumaUltimos2Digitos x < sumaUltimos2Digitos y = 1
+    | sumaUltimos2Digitos x == sumaUltimos2Digitos y = 0
+
+sumaUltimos2Digitos :: Int -> Int
+sumaUltimos2Digitos x = digitoDecenas x + digitoUnidades x
+
+digitoDecenas :: Int -> Int
+digitoDecenas x = div((mod x 100) - (digitoUnidades x)) 10
+
+digitoUnidades :: Int -> Int
+digitoUnidades x = mod x 10
+
+-- 9
+-- f4 = promedio
+-- f5 = promedio par
+

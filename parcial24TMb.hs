d4d453b04d67685af01188dcbb20ccf9968df660
devlinz <lean4405@gmail.com>
2024-05-08 12:35:10 -0300

    Terminado parcial 2024 TM 8/8

---
+++ parcial24TMb.hs
@@ -0,0 +1,44 @@
+module Parcial24TMb where
+type Mercaderias = [String]
+type Stock = [(String,Integer)]
+type Precios = [(String,Float)]
+
+-- Ejercicio 1
+generarStock :: Mercaderias -> Stock
+generarStock [] = []
+generarStock productos =  generarStockAux productos []
+
+generarStockAux :: Mercaderias -> Stock -> Stock
+generarStockAux [] stockContado = stockContado
+generarStockAux (producto1:restoProductos) stockContado = generarStockAux restoProductos (agregarProducto producto1 stockContado)
+
+agregarProducto :: String -> Stock -> Stock
+agregarProducto producto [] = [(producto,1)]
+agregarProducto producto ((producto1,stock1):restoStock)
+    | producto == producto1 = (producto1,stock1+1):restoStock
+    | otherwise = (producto1,stock1) : agregarProducto producto restoStock
+
+-- Ejercicio 2
+stockDeProducto :: Stock -> String -> Integer
+stockDeProducto [] _ = 0
+stockDeProducto ((producto1,stock1):restoStock) producto
+    | producto1 == producto = stock1
+    | otherwise = stockDeProducto restoStock producto
+
+-- Ejercicio 3
+dineroEnStock :: Stock -> Precios -> Float
+dineroEnStock [] _ = 0
+dineroEnStock ((producto1,stock1):restoStock) listaDePrecios = (fromIntegral stock1 * buscarPrecio listaDePrecios producto1) + dineroEnStock restoStock listaDePrecios
+
+buscarPrecio :: Precios -> String -> Float
+--buscarPrecio [] _ = 0 -- Innecesario, lista de precios siempre contiene precio
+buscarPrecio ((producto1,precio1):restoPrecios) producto
+    | producto1 == producto = precio1
+    | otherwise =  buscarPrecio restoPrecios producto
+
+-- Ejercicio 4
+aplicarOferta :: Stock -> Precios -> Precios
+aplicarOferta _ [] = []
+aplicarOferta listaDeStock ((producto1,precio1):restoPrecios)
+    | stockDeProducto listaDeStock producto1 > 10 = (producto1,precio1 * 0.80) : aplicarOferta listaDeStock restoPrecios
+    | otherwise =  (producto1,precio1) : aplicarOferta listaDeStock restoPrecios
